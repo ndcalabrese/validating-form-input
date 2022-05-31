4ef560eeb5967f4a8753a33c0635d7de0b784745
ndcalabrese <ndcalabrese@gmail.com>
2022-05-31 15:14:01 -0400

    Initial commit

---
+++ src/test/java/com/springtutorial/schedulingtaskstutorial/SchedulingTasksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springtutorial.schedulingtaskstutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchedulingTasksApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
